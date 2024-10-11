9c274680de1209d3388f934edce7f22b7f3f384d
Eleazar Alfriadez <ealfriadez@gmail.com>
2024-10-11 12:08:17 -0500

    Commit inicial

---
+++ src/test/java/pe/edu/unfv/courses/CursoArquitecturaHexagonalCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.unfv.courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoArquitecturaHexagonalCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
