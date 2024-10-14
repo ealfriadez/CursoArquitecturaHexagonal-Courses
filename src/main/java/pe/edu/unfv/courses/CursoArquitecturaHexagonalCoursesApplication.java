dabdcd2dfbb5c562b317597dfa4ed2e600c1208a
Eleazar Alfredo, ALFRIADEZ YRIARTE <ealfriadez@gmail.com>
2024-10-13 23:16:24 -0500

    Avanzando con el video nro 5 minuto 38.00

--- src/main/java/pe/edu/unfv/courses/CursoArquitecturaHexagonalCoursesApplication.java
+++ src/main/java/pe/edu/unfv/courses/CursoArquitecturaHexagonalCoursesApplication.java
@@ -2,7 +2,9 @@ package pe.edu.unfv.courses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class CursoArquitecturaHexagonalCoursesApplication {
 
